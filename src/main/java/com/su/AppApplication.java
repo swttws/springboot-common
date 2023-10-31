64ec5dd185d497c901a8c1345a7a97906116009f
swt <swtywmx@via.cool>
2023-10-31 22:15:20 +0800

    引入mybatisPlus模块

--- src/main/java/com/su/AppApplication.java
+++ src/main/java/com/su/AppApplication.java
@@ -1,6 +1,7 @@
 package com.su;
 
 import com.su.annotation.LoadFile;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2023/7/16 16:52
  */
 @SpringBootApplication
+@MapperScan("com.su.mapper")
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class,args);
